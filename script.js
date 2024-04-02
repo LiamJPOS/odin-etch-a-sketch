bb4e2e110c9f9cce839a5f449cc3a894169fbe15
liam <paledeepened@gmail.com>
2024-04-02 14:28:42 +0100

    Implements grid and event listener for mouseover on squares

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
 const mainContainer = document.getElementById('main-container');
-gridSize = 2 * 2;
+let gridSize = [50, 50];
+let rows = gridSize[0]
+let columns = gridSize[1]
 
-for(let i = 0; i < gridSize; i++){
-    let drawingSquare = document.createElement('div');
-    drawingSquare.classList.add("main-container__drawing-square");
-    mainContainer.appendChild(drawingSquare)
+for(let i = 0; i < rows; i++){
+    let row = document.createElement('div');
+    row.classList.add("main-container__row");
+    mainContainer.appendChild(row)
+    for(let i = 0; i < columns; i++){
+        let column = document.createElement('div');
+        column.classList.add("main-container__column");
+        row.appendChild(column);
+        column.addEventListener("mouseover", () => {
+            column.style.backgroundColor = "black";
+        })
+
+    }
 }
+
